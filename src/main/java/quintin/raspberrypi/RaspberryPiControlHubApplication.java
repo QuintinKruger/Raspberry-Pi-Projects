8c7636f2247aac93a73088fd1c152f92e3400476
Quintin Kruger <qkruger@jhb.dvt.co.za>
2020-08-15 14:20:27 +0200

    Raspnerry Pi and Java test project - toggle an LED

---
+++ src/main/java/quintin/raspberrypi/RaspberryPiControlHubApplication.java
@@ -0,0 +1,12 @@
+package quintin.raspberrypi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaspberryPiControlHubApplication {
+    public static void main(String... args){
+        SpringApplication.run(RaspberryPiControlHubApplication.class, args);
+    }
+
+}
---
+++ src/main/java/quintin/raspberrypi/controller/RaspberryPiController.java
@@ -0,0 +1,30 @@
+package quintin.raspberrypi.controller;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.PinState;
+import com.pi4j.io.gpio.RaspiPin;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RaspberryPiController {
+    private static GpioPinDigitalOutput pin;
+
+    @RequestMapping(value = "toggle", method = RequestMethod.POST)
+    public ResponseEntity<String> toggleLed(){
+        if (pin == null){
+            GpioController gpioController = GpioFactory.getInstance();
+            pin = gpioController.provisionDigitalOutputPin(RaspiPin.GPIO_14, "MyLED", PinState.LOW);
+        }
+        pin.toggle();
+
+        return new ResponseEntity<>("LED Toggled", HttpStatus.OK);
+
+    }
+
+}
